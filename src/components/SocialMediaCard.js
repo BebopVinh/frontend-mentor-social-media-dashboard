c3fef5da8be2a3cf964f02666d8a620ee3787d3a
bebopvinh <vinhdo07@gmail.com>
2020-05-19 13:03:08 -0700

    create card to render stats for each social media
    Co-authored-by: kyoung90 <kenneth.young90@gmail.com>

---
+++ src/components/SocialMediaCard.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+export default function SocialMediaCard({
+	title,
+	icon,
+	username,
+	following,
+	growth,
+}) {
+	const renderGrowth = () => {
+		return growth.positive ? (
+			<p className="positive-following flex-center">
+				<img src="/images/icon-up.svg" alt="up arrow icon" />
+				{growth.amount} Today
+			</p>
+		) : (
+			<p className="negative-following flex-center">
+				<img src="/images/icon-down.svg" alt="down arrow icon" />
+				{growth.amount} Today
+			</p>
+		)
+	}
+
+	const divStyles = () => {
+		return title === "instagram"
+			? {
+					borderTop: `4px solid`,
+					borderImage:
+						"linear-gradient(to right, var(--instagram-start), var(--instagram-end)",
+			  }
+			: { borderTop: `4px solid var(--${title})` }
+	}
+
+	return (
+		<div className={`social-media-card ${title}`}>
+			<p className="flex-center">
+				<img src={icon} alt="icon image" style={{ padding: "0px 8px" }} />
+				{username}
+			</p>
+			<div style={{ textAlign: "center" }}>
+				<p>{following.amount}</p>
+				<p>{following.type}</p>
+			</div>
+			{renderGrowth()}
+		</div>
+	)
+}
