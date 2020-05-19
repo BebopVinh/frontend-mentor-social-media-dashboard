cc67c01a24e350cb0e30d1945ac89a82c88125f9
bebopvinh <vinhdo07@gmail.com>
2020-05-19 12:59:00 -0700

    remove boilerplate code, render DashboardContainer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import DashboardContainer from "./containers/DashboardContainer"
+
+function App() {
+	return (
+		<div className="App">
+			<DashboardContainer />
+		</div>
+	)
+}
+
+export default App
