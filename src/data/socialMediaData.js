7577d243bf236e0ebe19f974dd973081c8300903
bebopvinh <vinhdo07@gmail.com>
2020-05-19 13:02:12 -0700

    created dummy data to render card
    Co-authored-by: kyoung90 <kenneth.young90@gmail.com>

---
+++ src/data/socialMediaData.js
@@ -0,0 +1,33 @@
+const facebook = {
+	title: "facebook",
+	icon: "/images/icon-facebook.svg",
+	username: "@nathanf",
+	following: { amount: "1987", type: "Followers" },
+	growth: { positive: true, amount: 12 },
+}
+
+const twitter = {
+	title: "twitter",
+	icon: "/images/icon-twitter.svg",
+	username: "@nathanf",
+	following: { amount: "1044", type: "Followers" },
+	growth: { positive: true, amount: 99 },
+}
+
+const instagram = {
+	title: "instagram",
+	icon: "/images/icon-instagram.svg",
+	username: "@realnathanf",
+	following: { amount: "11k", type: "Followers" },
+	growth: { positive: true, amount: 1099 },
+}
+
+const youtube = {
+	title: "youtube",
+	icon: "/images/icon-youtube.svg",
+	username: "Nathan F.",
+	following: { amount: "8239", type: "Subscribers" },
+	growth: { positive: false, amount: 144 },
+}
+
+export const socialMediaSites = [facebook, twitter, instagram, youtube]
