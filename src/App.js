62bf9c1877b815eb927098268bd291c7bfddd2a9
bebopvinh <vinhdo07@gmail.com>
2020-05-27 12:36:51 -0700

    Add rendering of OverviewContainer
    Co-authored-by: Kenneth Young <kenneth.young90@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./App.css"
 import DashboardContainer from "./containers/DashboardContainer"
+import OverviewContainer from "./containers/OverviewContainer"
 
 function App() {
 	return (
 		<div className="app-wrapper">
 			<div className="App">
 				<DashboardContainer />
+				<OverviewContainer />
 			</div>
 		</div>
 	)
